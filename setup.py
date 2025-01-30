f9b27ca1085b107d207768c16f579042ff1a00bf
helloginov <70898926+helloginov@users.noreply.github.com>
2025-01-30 13:24:49 +0300

    initial commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='vessel',
       version='0.0.1',
       description='Perdormance of tokamak equilibrium for reflectonetry purposes',
-      url='https://github.com/helloginov/vessel',
+      url='https://github.com/helloginov/vessel.git',
       author='Artyom Loginov',
       author_email='login.of.artyom@yandex.ru',
       license='unlicence',
